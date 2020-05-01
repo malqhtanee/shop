72c2c7b8c0ee166377c84391cc00c26f97204267
shaikha <d942a494867f6ca76d44e353bac882b1c988f1d6>
2020-05-01 20:02:32 +0300

    Initial Commit

---
+++ shop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shop
+//
+//  Created by shaikha alqhtane on 5/1/20.
+//  Copyright © 2020 shaikha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
